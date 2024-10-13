0f3105c7024b3e022a7058e712a8213401e5e52c
David Walter <david@kibo.school>
2024-10-13 10:48:51 -0700

    Initial commit (squashed)

---
+++ src/trivia.js
@@ -0,0 +1,76 @@
+const questions = [
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What is the last letter of the Greek alphabet?",
+        "correct_answer": "Omega",
+        "incorrect_answers": [
+            "Mu",
+            "Epsilon",
+            "Kappa"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "According to Sherlock Holmes, &quot;If you eliminate the impossible, whatever remains, however improbable, must be the...&quot;",
+        "correct_answer": "Truth",
+        "incorrect_answers": [
+            "Answer",
+            "Cause",
+            "Source"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What was the original name of the search engine &quot;Google&quot;?",
+        "correct_answer": "BackRub",
+        "incorrect_answers": [
+            "CatMassage",
+            "SearchPro",
+            "Netscape Navigator"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which country, not including Japan, has the most people of japanese decent?",
+        "correct_answer": "Brazil",
+        "incorrect_answers": [
+            "China",
+            "South Korea",
+            "United States of America"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "How many notes are there on a standard grand piano?",
+        "correct_answer": "88",
+        "incorrect_answers": [
+            "98",
+            "108",
+            "78"
+        ]
+    },
+    {
+        "category": "General Knowledge",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "In the MMO RPG &quot;Realm of the Mad God&quot;, what dungeon is widely considered to be the most difficult?",
+        "correct_answer": "The Shatter&#039;s",
+        "incorrect_answers": [
+            "Snake Pit",
+            "The Tomb of the Acient&#039;s",
+            "The Puppet Master&#039;s Theater"
+        ]
+    }
+]
+
+// your game code goes here
